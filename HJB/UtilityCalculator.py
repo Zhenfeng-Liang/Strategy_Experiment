64461f7386c7f6dd60f001736c626faf7b204d59
Jose <joseleungbit@gmail.com>
2016-06-12 17:26:19 -0400

    Added UtilityCalculator

---
+++ HJB/UtilityCalculator.py
@@ -0,0 +1,63 @@
+from math import exp
+
+class UtilityCalculator:
+
+	def __init__(self, gamma, uType, a = 1.0, b = 1.0):
+
+		self.a = a
+		self.b = b
+		self.gamma = gamma
+		self.oneOverGamma = 1.0 / gamma
+		self.uType = uType
+
+		if self.uType == "HARA" or self.uType == "CRRA":
+			self.kappa = self.oneOverGamma - 1.0
+		else if self.uType == "CARA":
+			self.kappa = -1.0
+
+	def Au(self, v):
+
+		if self.uType == "CARA":
+			res = self.gamma
+		else if self.uType == "CRRA":
+			res = self.gamma / v
+		else if self.uType == "HARA":
+			res = self.b / (self.a + self.b / self.gamma * v) 
+
+		return res
+
+	def U(self, v):
+
+		if self.uType == "CARA":
+			res = -exp(-self.gamma * v) / self.gamma
+		else if self.uType == "CRRA":
+			res = v ** (1.0 - self.gamma) / (1.0 - self.gamma)
+		else if self.uType == "HARA":
+			res = self.gamma / (1.0 - self.gamma) \
+			* (self.a + self.b / self.gamma * v) ** (1.0 - self.gamma)
+
+		return res
+
+	def UDer(self, v):
+
+		if self.uType == "CARA":
+			res = exp(-self.gamma * v)
+		else if self.uType == "CRRA":
+			res = v ** (-self.gamma)
+		else if self.uType == "HARA":
+			res = self.b * (self.a + self.b / self.gamma * v) ** (-self.gamma)
+
+		return res
+
+	def UDer2(self, v):
+
+		if self.uType == "CARA":
+			res = -self.gamma * exp(-self.gamma * v)
+		else if self.uType == "CRRA":
+			res = -self.gamma * v ** (-self.gamma - 1.0)
+		else if self.uType == "HARA":
+			res = -self.b ** 2 \
+			* (self.a + self.b / self.gamma * v) ** (-self.gamma - 1.0)
+
+		return res
+		
